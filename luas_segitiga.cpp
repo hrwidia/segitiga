c52217887dc00cb0c12a0d36d3561e9095b2e6f5
Hafiz Ramadhan <39754741+hrwidia@users.noreply.github.com>
2019-09-16 10:40:39 +0700

    Create luas_segitiga.cpp

---
+++ luas_segitiga.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+/*
+	Nama Lengkap : Hafiz Ramadhan
+	Nomor Induk Mahasiswa : [encrypted]
+*/
+using namespace std;
+
+int main(){
+	int alas, tinggi, luas;
+	cout<<"-- TEKNIK INFORMATIKA - 14 September 2019 (Menghitung Luas Segitiga) --\n";
+	cout<<"--------------------------------------------------------------------------\n";
+	cout<<"Masukan alas   = ";
+	cin>>alas;
+	cout<<"Masukan tinggi = ";
+	cin>>tinggi;
+	luas = alas * tinggi / 2;
+	cout<<"Luas segitiga = "<<luas;
+	cout<<"\n";
+	cout<<"--------------------------------\n";
+	cout<<"Nama Lengkap : Hafiz Ramadhan\n";
+	cout<<"Nomor Induk Mahasiswa : [encrypted]\n";
+}
